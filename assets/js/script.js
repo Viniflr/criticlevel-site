e21ab83c559dfce227a73adaf3312b33ab2a6543
Vini <limarezendev1@gmail.com>
2024-10-28 11:44:22 -0300

    scroll-animado

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ const myObserver = new IntersectionObserver((entries) => {
     entries.forEach( (entry) => {
         if(entry.isIntersecting) {
             entry.target.classList.add('show')
+            entry.target.classList.add('show-socios')
         } else {
             entry.target.classList.remove('show')
+            entry.target.classList.remove('show-socios')
         }
     })
 })
 
-const elements = document.querySelectorAll('.hidden')
+const elements = document.querySelectorAll('.hidden, .hidden-socios')
 
 elements.forEach((element) => myObserver.observe(element))
